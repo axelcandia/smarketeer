8a27a52a9941cad174389671d918fc05d02f8b91
Axl <axelcandia2609@gmail.com>
2016-01-31 12:55:10 -0300

    Pantalla de login
    Se arregla la imagen del login
    Se eliminan los 200 flags que sobraban
    Se comienza a ordenar los archivos que sobran

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ var port 	= process.env.PORT || 1337;
 var app     = express();
 
 //Comentar esta linea antes de subir
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Send to the index
 
 app.get('/', function (req, res) {
   res.sendFile(path.join( __dirname+ "/index.html"));
 });
+
 
 app.get('/login', function (req, res) {
   res.sendFile(path.join( __dirname+ "/Layout/Security/login.html"));
