fd4bd1afe62995c2aa971fcc6f7ebd69dc395adb
Axl <axelcandia2609@gmail.com>
2016-01-30 21:02:48 -0300

    Se agrega path
    Se arreglan algunos errores en el main

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //Modules
 var express = require("express");
+var path    = require("path");
 
 //Start directory
 var app     = express();
 
 //Send to the index
 app.get("/",function(req,res){
-	res.sendFile("main.html");
+	 res.sendFile(path.join( __dirname+ "/home.html"));
 });
+app.listen(3000);
