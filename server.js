b9788141e1fbd523120d7b2e7d611e4b0b4fe0ee
Axl <axelcandia2609@gmail.com>
2016-01-30 22:00:42 -0300

    Main
    Se arreglo el inicio

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ var path    = require("path");
 
 //Start directory
 var app     = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 //Send to the index
+
+
 app.get("/",function(req,res){
-	 res.sendFile(path.join( __dirname+ "/home.html"));
+	
+	 res.sendFile(path.join( __dirname+ "/main.html"));
 });
 app.listen(3000);
