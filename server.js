4b3c32c76c668d6609eeca8e37dbe00e44ab2c40
Axl <axelcandia2609@gmail.com>
2016-01-31 11:45:27 -0300

    Se inicia etapa de prueba con express
    Se sube un código de prueba para solucionar el problema de
    implementación con express

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-/*
-/
-/Modules
+
+//Modules
 var express = require("express");
 var path    = require("path");
-
+var port 	= process.env.PORT || 1337;
 //Start directory
-var app     = express();
-app.use(express.static(path.join(__dirname, 'public')));
-//Send to the index
+var app     = express(); 
 
 
-app.get("/",function(req,res){
+/*app.get("/",function(req,res){
 	
 	 res.sendFile(path.join( __dirname+ "/index.html"));
 });
-app.listen(3000);
+app.listen(port);
 */
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+app.get('/', function (req, res) {
+  res.send('Hello World, we are using express correctly!');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port!'+port);
+});
