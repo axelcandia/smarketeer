02233428227f5140f061ffb14aa59dd703202db7
Axl <axelcandia2609@gmail.com>
2016-04-18 17:29:01 -0300

    Se inicializa el seteo de IP del custoDimension

--- app/controllers/visits.server.controller.js
+++ app/controllers/visits.server.controller.js
@@ -8,7 +8,7 @@ var visits					     = require("./analytics.core.controller");
 
 exports.GetTotalVisits = function (req,res){
 	visits.GetAnalytics(req,res);  
-	CreateIpDimension(req,res);
+	GetIpDimension(req,res);
 }
 
 /***
@@ -17,29 +17,56 @@ exports.GetTotalVisits = function (req,res){
 * @Return true if it creates a new one
 * @Returns DeleteDimension if it was not able due to an error
 */
-function CreateIpDimension( req, res ){
+function GetIpDimension( req, res ){
 
-  CustomDimension.findOne({ id: req.user.google}, function(err, existingDimension) {
-    if(err)
-      console.log("ahooooo");
-    if(existingDimension)
-      console.log("Everything is awesome");
-    else{
-      var data = CustomDimension.findOne( { user: "asd" },
+  CustomDimension.findOne({},
                  { pages: { $elemMatch: { _id: "UA2-123123-123123" } } },
-                 function(err,data){
-                  console.log(JSON.stringify(data.pages[0].dimesionId));
-                 });
-      
-    }
+                function(err, existingDimension) {
+    if(err)
+      console.log("WE HAVE AN ERROR BUUUUUUU");
+    if(existingDimension){
+       if(existingDimension.pages[0].dimensionId)
+          return existingDimension.pages[0].dimensionId;
+       else
+        //Create a new dimension and push it in current status
+        SetIpDimension(existingDimension.pages[0].dimensionId, function(err,data){
+          console.log(data);
+        })
+    } 
       
   });
 }
-
 /**
-* @Receives the ID of dimension to delete
-* @Return true if delete == succes
+* @Receives: WebPropertyId
+* @Medium:   Parse the webProperty into accountId and make the insert
+* @Returns: If err
+*              return err 
+*          else execute callback
 */
-function DeleteDimension( req, res ){
+function SetIpDimension( webProperty,callback ){
+  console.log(webProperty);
+  var account = webProperty.split("-");
+  var params={
+        "accountId":account[1],
+        "webPropertyId":webProperty,
+        "resource":{
+          "name":"AIPs",
+          "scope":"USER",
+          "active":true
+        }
+      };
+      //Insert the new customDimension
+      analytics.management.customDimensions.insert(params, function(err,data){
+        if(err) console.log(err)
+        else
+          console.log(data);
+          callback(null,data);
+      });
+
+}
+function SaveIpDimension( ){
+
+}
+function DeleteIpDimension( req, res ){
 
 }
