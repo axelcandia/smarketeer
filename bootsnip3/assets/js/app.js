3c8e549d47d580dbc1a03d3bd967bcd648554dc9
Axl <axelcandia2609@gmail.com>
2016-05-03 16:51:14 -0300

    Se agregan todos los personalizados

--- bootsnip3/assets/js/app.js
+++ bootsnip3/assets/js/app.js
@@ -2,44 +2,33 @@ define([
        "jquery" , "underscore" , "backbone"
        , "collections/snippets" , "collections/my-form-snippets"
        , "views/tab" , "views/my-form"
-       , "text!data/input.json", "text!data/radio.json", "text!data/select.json", "text!data/buttons.json"
-       , "text!templates/app/render.html",  "text!templates/app/about.html", 
+       , "text!data/input.json", "text!data/radio.json", "text!data/predeterminado.json"
+       , "text!templates/app/render.html" 
 ], function(
   $, _, Backbone
   , SnippetsCollection, MyFormSnippetsCollection
   , TabView, MyFormView
-  , inputJSON, radioJSON, selectJSON, buttonsJSON
-  , renderTab, aboutTab
+  , inputJSON, radioJSON, predeterminadoJSON
+  , renderTab
 ){
   return {
     initialize: function(){
-
-      //Bootstrap tabs from json.
       new TabView({
-        title: "Input"
+        title: "Predeterminado"
+        , collection: new SnippetsCollection(JSON.parse(predeterminadoJSON))
+      }); 
+      new TabView({
+        title: "Texto"
         , collection: new SnippetsCollection(JSON.parse(inputJSON))
       });
       new TabView({
-        title: "Radios / Checkboxes"
+        title: "Opciones"
         , collection: new SnippetsCollection(JSON.parse(radioJSON))
       });
       new TabView({
-        title: "Select"
-        , collection: new SnippetsCollection(JSON.parse(selectJSON))
-      });
-      new TabView({
-        title: "Buttons"
-        , collection: new SnippetsCollection(JSON.parse(buttonsJSON))
-      });
-      new TabView({
-        title: "Rendered"
+        title: "Codigo"
         , content: renderTab
       });
-      new TabView({
-        title: "About"
-        , content: aboutTab
-      });
-
       //Make the first tab active!
       $("#components .tab-pane").first().addClass("active");
       $("#formtabs li").first().addClass("active");
