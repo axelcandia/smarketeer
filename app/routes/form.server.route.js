b3cccbe272eab3101d6f2cfc4b95b8553764e144
Axl <axelcandia2609@gmail.com>
2016-05-24 17:47:29 -0300

    Se recibe la informacion de los CORS

--- app/routes/form.server.route.js
+++ app/routes/form.server.route.js
@@ -1,11 +1,14 @@
 var formController = require("../controllers/form.server.controller");
+//var cors  		   = require('cors'); 
+var cors = require('cors')
 
 module.exports = function(app) {
+	app.options('/ReceiveForms*', cors());
 	app.get('/home/allforms',formController.RenderGetAllForms); 
 	app.get('/home/forms/formbuilder/:name',formController.RenderFormBuilder);
 	app.get('/home/forms/formbuilder/:name/:id',formController.RenderFormBuilder);
 	app.post("/home/forms/UpdateForm*",formController.UpdateForm);
-	app.post("/ReceiveForms*",formController.ReceiveForms);
+	app.post("/ReceiveForms*", cors(),formController.ReceiveForms);
 	app.post("/home/forms/eliminar/:id", formController.DeleteForm );
 	app.post("/home/forms/clonar/:id", formController.CloneForm );
 }; 
---
+++ public/forms/4X3!.js
@@ -0,0 +1,81 @@
+/**
+*This function
+*/  
+
+function Send(form_id){
+	var values = {};
+	var smkt  =  document.getElementById(form_id).getElementsByClassName('SmarketeerField');
+	values["pkw_id"]= visitor_id;
+	for(var i=0;i< smkt.length;i++){ 
+		var name= smkt[i].name;
+		if( smkt[i].value == "-Seleccione una opcion-"){
+			smkt[i].value="";
+		} 
+		if( (smkt[i].type=="checkbox" || smkt[i].type=="radio") ){
+			if(smkt[i].checked) 
+				values[name]=smkt[i].value;  
+		}
+		else if(smkt[i].tagName == "SELECT"){
+			values[name]=getSelectValues(smkt[i]);  
+		}
+		else{
+			values[name]=smkt[i].value; 
+		}
+			
+	}
+	console.log(values);
+	ajax(values);
+	
+} 
+
+function ajax(values) {
+
+    var xmlHTTP;
+
+    if (window.XMLHttpRequest) { 
+        xmlHTTP = new XMLHttpRequest();
+    } else { 
+        xmlHTTP = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xmlHTTP.onreadystatechange = function () {
+        if (xmlHTTP.readyState == 4 && xmlHTTP.status == 200) {
+            // do whatever it is you want to do
+        }
+    }
+
+    //Serialize the data
+    var queryString = JSON.stringify(values);
+    console.log(queryString);
+
+    xmlHTTP.open("POST", "http:localhost:1337/ReceiveForms", true); 
+    xmlHTTP.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlHTTP.send(queryString);
+}
+
+
+/**
+* GEt the selected values from a list.
+*/
+function getSelectValues(select) {
+  var result = [];
+  var options = select && select.options;
+  var opt;
+
+  for (var i=0, iLen=options.length; i<iLen; i++) {
+    opt = options[i];
+
+    if (opt.selected) {
+      result.push(opt.value || opt.text);
+    }
+  }
+  return result;
+}
+
+function Array2Arg() {
+  var args = arguments;
+  for(var i = 0; i < args; ++i) {
+    var argument = args[i];
+    console.log("asd"+args[i]);
+  }
+}
