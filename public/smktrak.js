dc3c7934f5e48e7c0001b09d97e83a2f368d8b7d
Axl <axel@smarketeer.io>
2016-06-27 15:16:00 -0300

    Update smktrak.js

--- public/smktrak.js
+++ public/smktrak.js
@@ -1,4 +1,4 @@
-var website_id= parseInt(document.currentScript.id);
+var website_id= document.currentScript.id;
 	  console.log("our id is " +website_id );
 	  var _paq = _paq || [];
 	  var tracker;
@@ -6,11 +6,11 @@ var website_id= parseInt(document.currentScript.id);
 	  var _paqid; 
 	  _paq.push(['enableLinkTracking']);
 	  _paq.push(['setConversionAttributionFirstReferrer', true]);
-	  (function() {  
-	    var u="//52.165.38.47/"; 
+	  (function() { 
+	    var u="http://52.165.38.47/";
 	    _paq.push([ function() {  
 			visitor_id = this.getVisitorId(); 
-			//Cookie do not exist create on
+			//Cookie do not exist create one
 			var cname="smkt_"+website_id;
 			var username=getCookie(cname);
 			console.log(username);
@@ -22,25 +22,24 @@ var website_id= parseInt(document.currentScript.id);
 				  url: "http://smarketeer.azurewebsites.net/GetVisitorId",
 				  data: {"id":this.getVisitorId()},
 				  success: function(data){
-				  	console.log("Succes!:"+data)
+				  	console.log(data);
 				  	setCookie(website_id,visitor_id,400);
 				  	_paq.push(['setUserId', visitor_id]);
 				  	_paq.push(['setConversionAttributionFirstReferrer', true]);
-					_paq.push(['trackPageView']); 
-				    _paq.push(['setTrackerUrl', u+'piwik.php']);
-				    _paq.push(['setSiteId', website_id]);  
+					_paq.push(['trackPageView']);   
 				  }
 				  });
 
 			}
 			else{ 
+					console.log("HOLA");
 					_paq.push(['setUserId', username]);
 				  	_paq.push(['setConversionAttributionFirstReferrer', true]);
 					_paq.push(['trackPageView']);  
-				    _paq.push(['setTrackerUrl', u+'piwik.php']);
-				    _paq.push(['setSiteId', website_id]); 
+				    
 			}
-			   
+			   _paq.push(['setTrackerUrl', u+'piwik.php']);
+				    _paq.push(['setSiteId', website_id]); 
 
 			}]); 
 	    
