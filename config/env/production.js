0b1e3cedcbeb347b3a8aa81f87be0c08fd6ed2ca
axelcandia <axelcandia2609@gmail.com>
2016-10-17 21:32:26 -0300

    Se arregla redireccion

--- config/env/production.js
+++ config/env/production.js
@@ -7,18 +7,18 @@ module.exports = {
 	facebook : {
 		clientID 		: '1539348089726612', // your App ID
 		clientSecret 	: '3cf68a04a1859ab4ab158a54ef34f4fe', // your App Secret
-		callbackURL 	: 'http://smarketeer.io/auth/facebook/callback'
+		callbackURL 	: 'https://app.smarketeer.io/auth/facebook/callback'
 	},
 	twitter : {
 		consumerKey		: '5H9sw7LGU9ZQekpdl1HKH7ob2',
 		consumerSecret 	: 'il8xYuImWL2KiTlo8ToC7rQSAY2DIUIHYpgz6s8UiCaVqvl452',
-		callbackURL 	: 'http://smarketeer.io/auth/twitter/callback'
+		callbackURL 	: 'https://app.smarketeer.io/auth/twitter/callback'
 	},
 
 	google : {
 		clientID 		: '881118608327-96r85v4lv03mo8amu62ah289qqbrrpal.apps.googleusercontent.com',
 		clientSecret 	: "MrFvZ7oJX2v2z765ahnl429T",
-		callbackURL 	: 'http://smarketeer.io/auth/google/callback'
+		callbackURL 	: 'https://app.smarketeer.io/auth/google/callback'
 	},
 	piwik:{
 		user             : "Axel",
